25afa20c95b4c8905ba821a23904b76160b1db0a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-07 15:24:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,17 @@ class PostsController < ApplicationController
     @authors = Author.all
     
     if !params[:author].blank?
-      @posts = Post.where(author: params[:author])
-      
-    elsif !params[:date].blank?
-      if params[:date] == "Today"
-        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+        @posts = Post.by_author(params[:author])
+      elsif !params[:date].blank?
+        if params[:date] == "Today"
+          @posts = Post.from_today
+        else
+          @posts = Post.old_news
+        end
       else
-        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+        # if no filters are applied, show all posts
+        @posts = Post.all
       end
-    else
-      @posts = Post.all
-    end 
   end
 
   def show
